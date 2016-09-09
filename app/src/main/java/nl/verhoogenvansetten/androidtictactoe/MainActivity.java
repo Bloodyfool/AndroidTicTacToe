47b149ddeec0ec8cdaad9c11608dedf459d5c866
Bas Verhoog <basverhoog@live.nl>
2016-09-09 16:58:54 +0200

    Added Gradle files?

--- app/src/main/java/nl/verhoogenvansetten/androidtictactoe/MainActivity.java
+++ app/src/main/java/nl/verhoogenvansetten/androidtictactoe/MainActivity.java
@@ -1,5 +1,7 @@
 package nl.verhoogenvansetten.androidtictactoe;
 
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
