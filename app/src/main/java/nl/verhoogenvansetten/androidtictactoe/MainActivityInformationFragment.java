c0607c436c3149bc7f7db7046ff3227c966fd432
kkkkk317 <giyen10@naver.com>
2016-09-13 12:12:23 +0200

    layout change

--- app/src/main/java/nl/verhoogenvansetten/androidtictactoe/MainActivityInformationFragment.java
+++ app/src/main/java/nl/verhoogenvansetten/androidtictactoe/MainActivityInformationFragment.java
@@ -5,18 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityInformationFragment extends Fragment {
 
+
     public MainActivityInformationFragment() {
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        return inflater.inflate(R.layout.fragment_information, container, false);
     }
+
 }
